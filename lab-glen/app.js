d3f4c2016b24020cf3b6a0c3e8c1849345258bf0
galang pham <phamgalang@gmail.com>
2017-05-23 21:25:28 -0700

    initial

---
+++ lab-glen/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const cowsayApp = angular.module('cowsayApp', [])
+
+cowsayApp.controller('CowsayController', ['$log', 'CowsayController'])
+
+function CowsayController($log) {
+  $log.debug('#CowsayController')
+
+  $log.log('check this out', this)
+
+  this.title = 'Welcome to COW VILLE HAHAHAHAHHA'
+  this.history = []
+
+  cowsay.list((err, cows) => {
+    this.cowfiles = cows
+    this.current = this.cowfiles[0]
+  })
+
+  this.update = function(input) {
+    $log.debug('#update')
+    return cowsay.say({text:input || 'moooooo', f:this.current})
+  }
+
+  this.speak = function(input) {
+    $log.debug('#speak')
+    this.spoken = this.update(input)
+    this.history.push(this.spoken)
+  }
+
+  this.undo = function() {
+    $log.debug('#undo')
+    let temp = this.history.pop()
+    this.spoken = temp || ''
+  }
+}
+
+cowsayApp.controller('Navigation', ['$log', NavigationController])
+
+function NavigationController($log) {
+  $log.debug('#NavigationController')
+
+  this.routes = [
+    {
+      name: 'home',
+      url: '/home'
+    },
+    {
+      name: 'about',
+      url: '/about'
+    },
+    {
+      name: 'contact',
+      url: '/contact'
+    }
+  ]
+}
